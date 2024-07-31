97cd06ea6cb1478e23e8097583094ce1cc36d54e
Martin Hlavňa <martin.hlavna@4q.sk>
2024-07-31 11:34:24 +0200

    Version bump

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -35,7 +35,7 @@ EDITOR_LOGO_HEIGHT = 300
 EDITOR_LOGO_WIDTH = 300
 NLP_DEBOUNCE_LENGTH = 500
 ENABLE_DEBUG_DEP_IMAGE = False
-VERSION = "0.8.2 Alfa"
+VERSION = "0.8.3 Alfa"
 
 with open(Utils.resource_path(os.path.join('data_files', 'pos_tag_translations.json')), 'r', encoding='utf-8') as file:
     POS_TAG_TRANSLATIONS = json.load(file)
