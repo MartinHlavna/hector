b4dc0f00cb603459436cfe51550f95f4baa1f8c1
Martin Hlavňa <martin.hlavna@4q.sk>
2024-11-08 15:26:13 +0100

    #36 - Passing github token for application tests

--- hector.py
+++ hector.py
@@ -11,37 +11,38 @@ from src.gui.main_window import MainWindow
 from src.gui.splash_window import SplashWindow
 from src.utils import Utils
 
-if platform.system() == 'Windows':
-    ctypes.windll.shcore.SetProcessDpiAwareness(True)
+if __name__ == "__main__":
+    if platform.system() == 'Windows':
+        ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("--github_token", help="Run with this github token for all github calls")
-parser.add_argument("--github_user", help="Run with this github token for all github calls")
+    parser.add_argument("--github_token", help="Run with this github token for all github calls")
+    parser.add_argument("--github_user", help="Run with this github token for all github calls")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-root = ThemedTk(theme="clam")
-root.title("Hector")
-photo = tk.PhotoImage(file=Utils.resource_path('images/hector-icon.png'))
-root.wm_iconphoto(True, photo)
-splash = SplashWindow(root)
-splash.update_status("sťahujem a inicializujem jazykový model...")
-nlp = Service.initialize_nlp()
-splash.update_status("sťahujem a inicializujem slovník...")
-dictionaries = Service.initialize_dictionaries(github_token=args.github_token, github_user=args.github_user)
-splash.update_status("sťahujem pandoc...")
-Service.download_pandoc()
-splash.update_status("kontrolujem aktualizácie...")
-has_available_update = False
-build_info = Utils.get_build_info()
-if True:
-    has_available_update = Utils.check_updates(VERSION, True,
-                                               github_token=args.github_token, github_user=args.github_user)
-if build_info['channel'] == "stable":
-    has_available_update = Utils.check_updates(VERSION, False,
-                                               github_token=args.github_token, github_user=args.github_user)
-splash.update_status("inicializujem textový processor...")
-splash.close()
-main_window = MainWindow(root, nlp, dictionaries["spellcheck"], dictionaries["thesaurus"], has_available_update)
-main_window.start_main_loop()
+    root = ThemedTk(theme="clam")
+    root.title("Hector")
+    photo = tk.PhotoImage(file=Utils.resource_path('images/hector-icon.png'))
+    root.wm_iconphoto(True, photo)
+    splash = SplashWindow(root)
+    splash.update_status("sťahujem a inicializujem jazykový model...")
+    nlp = Service.initialize_nlp()
+    splash.update_status("sťahujem a inicializujem slovník...")
+    dictionaries = Service.initialize_dictionaries(github_token=args.github_token, github_user=args.github_user)
+    splash.update_status("sťahujem pandoc...")
+    Service.download_pandoc()
+    splash.update_status("kontrolujem aktualizácie...")
+    has_available_update = False
+    build_info = Utils.get_build_info()
+    if build_info['channel'] == "beta":
+        has_available_update = Utils.check_updates(VERSION, True,
+                                                   github_token=args.github_token, github_user=args.github_user)
+    if build_info['channel'] == "stable":
+        has_available_update = Utils.check_updates(VERSION, False,
+                                                   github_token=args.github_token, github_user=args.github_user)
+    splash.update_status("inicializujem textový processor...")
+    splash.close()
+    main_window = MainWindow(root, nlp, dictionaries["spellcheck"], dictionaries["thesaurus"], has_available_update)
+    main_window.start_main_loop()
