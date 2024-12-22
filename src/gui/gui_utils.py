0cabcfd356cf95ecbf24e396343196978ff0f577
MartinHlavna <mato.hlavna@gmail.com>
2024-12-22 18:32:12 +0100

    #75 - Review for gui_utils.py and tooltip.py

--- src/gui/gui_utils.py
+++ src/gui/gui_utils.py
@@ -1,16 +1,7 @@
-import os
-import string
-
 from PIL import ImageFont, ImageDraw, Image, ImageTk
-
-from src.const.paths import RUN_DIRECTORY
 
 
 class GuiUtils:
-    @staticmethod
-    def resource_path(relative_path: string):
-        return os.path.join(RUN_DIRECTORY, relative_path)
-
     @staticmethod
     def fa_image(font, background, foreground, char, size, padding=2):
         img = Image.new("L", (size, size), background)
--- src/gui/tooltip.py
+++ src/gui/tooltip.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from src.const.colors import ACCENT_COLOR, PANEL_TEXT_COLOR
 
 
+# TOOLTIP THAT SHOWS IN APPLICATION
 class Tooltip:
     def __init__(self, root):
         self.root = root
@@ -14,45 +15,48 @@ class Tooltip:
                               wraplength=240,
                               justify="left", padx=5, pady=5)
         self.label.pack()
-        self._after_id = None  # To track scheduled calls
+        self._timer_id = None  # To track scheduled calls
 
+    # PUBLIC SHOW METHOD. SCHEDULES TOOLTIP TO SHOW AFTER 500ms
+    # CANCELS ANY PREVIOUS SCHEDULED SHOWS
     def show(self, text, x, y):
-        # Cancel any pending tooltip show/hide operations
-        if self._after_id:
-            self.root.after_cancel(self._after_id)
-            self._after_id = None
+        # CANCEL ANY PENDING TOOLTIP SHOW/HIDE OPERATIONS
+        if self._timer_id:
+            self.root.after_cancel(self._timer_id)
+            self._timer_id = None
 
-        # Store the after ID to manage cancelation
-        self._after_id = self.root.after(500, lambda t=text, _x=x, _y=y: self.delayed_show(t, _x, _y))
+        # STORE THE AFTER ID TO MANAGE CANCELLATION
+        # SCHEDULE SHOWING THE TOOLTIP WITH A 500MS DELAY
+        self._timer_id = self.root.after(500, lambda t=text, _x=x, _y=y: self._show_debounced(t, _x, _y))
 
-        # Schedule showing the tooltip with a 500ms delay
+    # PUBLIC HIDE METHOD. IMMEDIATELY HIDES TOOLTIP AND CANCELS ANY REQUEST SHOW CALLS
+    def hide(self):
+        # cancel any pending tooltip show operations
+        if self._timer_id:
+            self.root.after_cancel(self._timer_id)
+            self._timer_id = None
 
-    def delayed_show(self, text, x, y):
-        # Get screen dimensions
+        # IMMEDIATELY HIDE THE TOOLTIP
+        self.toplevel.withdraw()
+
+    # PRIVATE CALLBACK, THAT ACTUALLY SHOWS TOOLTIP
+    def _show_debounced(self, text, x, y):
+        # GET SCREEN DIMENSIONS
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
 
-        # Estimate the tooltip size
+        # ESTIMATE THE TOOLTIP SIZE
         self.label.config(text=text)
-        self.toplevel.update_idletasks()  # Ensures dimensions are accurate after setting text
+        self.toplevel.update_idletasks()  # ENSURES DIMENSIONS ARE ACCURATE AFTER SETTING TEXT
         tooltip_width = self.toplevel.winfo_reqwidth()
         tooltip_height = self.toplevel.winfo_reqheight()
 
-        # Adjust x and y if the tooltip would go off screen
+        # ADJUST X AND Y IF THE TOOLTIP WOULD GO OFF SCREEN
         if x + 20 + tooltip_width > screen_width:
             x = screen_width - tooltip_width - 20
         if y + 20 + tooltip_height > screen_height:
             y = screen_height - tooltip_height - 20
 
-        # Position the tooltip slightly offset from the mouse pointer
+        # POSITION THE TOOLTIP SLIGHTLY OFFSET FROM THE MOUSE POINTER
         self.toplevel.geometry(f"+{x + 20}+{y + 20}")
         self.toplevel.deiconify()
-
-    def hide(self):
-        # Cancel any pending tooltip show/hide operations
-        if self._after_id:
-            self.root.after_cancel(self._after_id)
-            self._after_id = None
-
-        # Immediately hide the tooltip
-        self.toplevel.withdraw()
